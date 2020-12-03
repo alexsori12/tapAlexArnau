ea7bdd1230f6708c9be340525401f75958fdeb43
Arnau F Llaberia <arnaufrancesc.llaberia@estudiants.urv.cat>
2020-12-03 19:41:16 +0100

    Primeres proves MailStore

--- TAP-1/src/TASCA0_TAP/MailSystem.java
+++ TAP-1/src/TASCA0_TAP/MailSystem.java
@@ -1,8 +1,19 @@
 package TASCA0_TAP;
 
+import java.util.ArrayList;
+
 public class MailSystem {
 
+    private ArrayList<MailBox> Usermailbox;
 
+    public MailSystem(){};
 
+    public void addUser(User user){Usermailbox.add(new MailBox(user));}
+
+    public ArrayList<Message> getAllMissages(){
+        ArrayList<Message> returned;
+        for(MailBox m: Usermailbox)
+            for()
+    }
 
 }
